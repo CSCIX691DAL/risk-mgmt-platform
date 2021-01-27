70b32ed72a5d03a085670a126fd129f151a89105
Fred Winters <fr553053@dal.ca>
2021-01-27 10:55:56 -0400

    Fixes for Heroku deployment

---
+++ server.js
@@ -0,0 +1,15 @@
+/* Source for the following on how to set up a Angular deployment via Heroku:
+ * Olutunmbi Banto - https://itnext.io/how-to-deploy-angular-application-to-heroku-1d56e09c5147 */
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./dist/risk-management-platform'));
+
+app.get('/*', function(req,res) {
+    res.sendFile('index.html', {root: 'dist/risk-management-platform/'} );
+});
+
+// Attempt to listen on the Heroku env port variable, fall back to 8080 otherwise.
+app.listen(process.env.PORT || 8080);
