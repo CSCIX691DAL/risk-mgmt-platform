4b9297be3401f39c0629b96682f28734c575245e
Ryan Alcorn <ryanalcorn2@gmail.com>
2021-02-26 11:47:08 -0400

    Added input for treatmentPlanItem

--- src/app/treatment-plan/treatment-plan-item/treatment-plan-item.component.ts
+++ src/app/treatment-plan/treatment-plan-item/treatment-plan-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {TreatmentPlanModel} from '../treatment-plan.model';
 
 @Component({
   selector: 'app-treatment-plan-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./treatment-plan-item.component.css']
 })
 export class TreatmentPlanItemComponent implements OnInit {
+
+  @Input() treatmentPlanItem: TreatmentPlanModel;
 
   constructor() { }
 
