c81304963cf541ab540e3b992ed887c3fdbc442e
Ryan Alcorn <ryanalcorn2@gmail.com>
2021-03-05 22:28:14 -0400

    Created modal to add user to organization

---
+++ src/app/admin-dash/orgs-view/orgs-add-user/orgs-add-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orgs-add-user',
+  templateUrl: './orgs-add-user.component.html',
+  styleUrls: ['./orgs-add-user.component.css']
+})
+export class OrgsAddUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
