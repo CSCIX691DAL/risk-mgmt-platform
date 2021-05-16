b6cf32aabdc905ad81f10cc5f980c6b684478581
AlcornRyan <ry737126@dal.ca>
2021-05-16 11:23:18 -0300

    Project now pointing to firestore database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyD-vlb6CLl8Lq25ykjHsHyhsQEOfbR-xi4',
-    authDomain: 'risk-management-platform-a1831.firebaseapp.com',
-    projectId: 'risk-management-platform-a1831',
-    storageBucket: 'risk-management-platform-a1831.appspot.com',
-    messagingSenderId: '489476723459',
-    appId: '1:489476723459:web:bef7cf2d4c90352d724f36'
+    apiKey: 'AIzaSyCJd6LltLEd84kAmt00ChFfHUMAYVXGAbw',
+    authDomain: 'risk-management-platform-19952.firebaseapp.com',
+    projectId: 'risk-management-platform-19952',
+    storageBucket: 'risk-management-platform-19952.appspot.com',
+    messagingSenderId: '702095689563',
+    appId: '1:702095689563:web:5721fc55036daac2927c7e'
   }
 };
 
