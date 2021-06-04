6e8247d910e02070fe9c88b431dda3142f513ccf
vrund17 <84090279+vrund17@users.noreply.github.com>
2021-06-04 14:02:35 -0300

    team page content

--- src/app/landing/information/team/team-s21/team-s21.component.ts
+++ src/app/landing/information/team/team-s21/team-s21.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-team-s21',
-    templateUrl: './team-s21.component.html',
-    styleUrls: ['./team-s21.component.css']
+  selector: 'app-team-s21',
+  templateUrl: './team-s21.component.html',
+  styleUrls: ['./team-s21.component.css']
 })
-
 export class TeamS21Component implements OnInit {
-    
-    constructor() { } 
 
-    ngOnInit(): void {
-    } 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }
