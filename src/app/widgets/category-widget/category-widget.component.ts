e63077e0ebe085ceabd91e5989f5ce50c8ef4dd0
AlcornRyan <ry737126@dal.ca>
2021-07-13 12:16:36 -0300

    Reformatting risk category widget. Adjusted HTML and added comments to typescript file.

--- src/app/widgets/category-widget/category-widget.component.ts
+++ src/app/widgets/category-widget/category-widget.component.ts
@@ -10,20 +10,24 @@ export class CategoryWidgetComponent implements OnInit {
 
   constructor(public categoryService: CategoryService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
+  // Returns count of Financial risk categories from categoryService
   public getFinancialCount(whichCount: number): number {
     return this.categoryService.countRiskCategories(whichCount);
   }
 
+  // Returns count of Strategic risk categories from categoryService
   public getStrategicCount(whichCount: number): number {
     return this.categoryService.countRiskCategories(whichCount);
   }
 
+  // Returns count of Operational risk categories from categoryService
   public getOperationalCount(whichCount: number): number {
     return this.categoryService.countRiskCategories(whichCount);
   }
 
+  // Returns count of Hazard risk categories from categoryService
   public getHazardCount(whichCount: number): number {
     return this.categoryService.countRiskCategories(whichCount);
   }
