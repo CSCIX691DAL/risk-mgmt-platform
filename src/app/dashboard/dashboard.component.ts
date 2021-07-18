4bb7a13067331d5ebf83380fa2f786b635f17284
AlcornRyan <ry737126@dal.ca>
2021-07-18 13:13:29 -0300

    DB documentation for Dashboard page.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
 }
